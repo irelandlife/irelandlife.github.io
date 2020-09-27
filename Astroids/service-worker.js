06422a2a8cd5ecf12cc002b917a265be7992ac96
Robert Ireland <rgireland@ucdavis.edu>
2020-09-27 15:40:04 -0700

    Add files via upload

--- Astroids/service-worker.js
+++ Astroids/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2561d4528b39d8669e1415b57f5c7724.js"
+  "/Astroids/index.html/precache-manifest.9430b4e5112ded92d96b2f7f3eb8ebc5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Astroids/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
