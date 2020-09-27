971bcc708920255d47f9833934ec509952610deb
Robert Ireland <rgireland@ucdavis.edu>
2020-09-27 15:38:13 -0700

    Update precache-manifest.2561d4528b39d8669e1415b57f5c7724.js

--- Astroids/precache-manifest.2561d4528b39d8669e1415b57f5c7724.js
+++ Astroids/precache-manifest.2561d4528b39d8669e1415b57f5c7724.js
@@ -1,58 +1,58 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b1b01b068e51540fef4d361436b5a9ff",
-    "url": "/index.html"
+    "revision": "3f8475634c67a4b8a1957d9335bd752d",
+    "url": "/Astroids/index.html/index.html"
   },
   {
-    "revision": "5096b3aa3ca753bfce09",
-    "url": "/static/js/2.d5ad6835.chunk.js"
+    "revision": "23693b53ecc15d105812",
+    "url": "/Astroids/index.html/static/js/2.84fa08b3.chunk.js"
   },
   {
     "revision": "f5a5ff1f96889814dd83b6008159ed69",
-    "url": "/static/js/2.d5ad6835.chunk.js.LICENSE.txt"
+    "url": "/Astroids/index.html/static/js/2.84fa08b3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "99a3b9bad92db9c01f5f",
-    "url": "/static/js/main.9a89c9dd.chunk.js"
+    "revision": "bd760e7f8da9ec507425",
+    "url": "/Astroids/index.html/static/js/main.980129bf.chunk.js"
   },
   {
-    "revision": "5193a261c24b13923233",
-    "url": "/static/js/runtime-main.c8364518.js"
+    "revision": "7d11e2ac5cd941ae365c",
+    "url": "/Astroids/index.html/static/js/runtime-main.fff3f841.js"
   },
   {
     "revision": "a93d8e1a89f9100d7e04c98ebcbc0933",
-    "url": "/static/media/bg.a93d8e1a.mp3"
+    "url": "/Astroids/index.html/static/media/bg.a93d8e1a.mp3"
   },
   {
     "revision": "8d516cce9b80ee257214da40ace5c474",
-    "url": "/static/media/click.8d516cce.mp3"
+    "url": "/Astroids/index.html/static/media/click.8d516cce.mp3"
   },
   {
     "revision": "12d1603454265a90269ca13e0d6c04b2",
-    "url": "/static/media/earth.12d16034.jpg"
+    "url": "/Astroids/index.html/static/media/earth.12d16034.jpg"
   },
   {
     "revision": "e896d081c42d144db443a0faeea36cf8",
-    "url": "/static/media/engine.e896d081.mp3"
+    "url": "/Astroids/index.html/static/media/engine.e896d081.mp3"
   },
   {
     "revision": "bdb080913490e33da93bb6510d530d4d",
-    "url": "/static/media/engine2.bdb08091.mp3"
+    "url": "/Astroids/index.html/static/media/engine2.bdb08091.mp3"
   },
   {
     "revision": "5c5ce6974ee4192d9ecbc74242daa4ce",
-    "url": "/static/media/explosion.5c5ce697.mp3"
+    "url": "/Astroids/index.html/static/media/explosion.5c5ce697.mp3"
   },
   {
     "revision": "a97c8e670e85efbd49691d7e343da830",
-    "url": "/static/media/laser.a97c8e67.mp3"
+    "url": "/Astroids/index.html/static/media/laser.a97c8e67.mp3"
   },
   {
     "revision": "8e294414ed8fefd6cc5fe6480adeaa7b",
-    "url": "/static/media/moon.8e294414.png"
+    "url": "/Astroids/index.html/static/media/moon.8e294414.png"
   },
   {
     "revision": "5699f1c4aff5845551ef0ba1a7ad31f8",
-    "url": "/static/media/warp.5699f1c4.mp3"
+    "url": "/Astroids/index.html/static/media/warp.5699f1c4.mp3"
   }
 ]);
