2b1df565d4ccd6ad258ea3500c5adf2e4f3be933
Robert Ireland <rgireland@ucdavis.edu>
2020-09-30 18:00:58 -0700

    Add files via upload

---
+++ precache-manifest.21bf8e991d19fff5a4ef90e020e1d72a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e590f76c27bfdf550b5679c9df96b4ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8781f82ef39be8e69c8",
+    "url": "/static/js/2.2af06ece.chunk.js"
+  },
+  {
+    "revision": "f5a5ff1f96889814dd83b6008159ed69",
+    "url": "/static/js/2.2af06ece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd760e7f8da9ec507425",
+    "url": "/static/js/main.980129bf.chunk.js"
+  },
+  {
+    "revision": "5193a261c24b13923233",
+    "url": "/static/js/runtime-main.c8364518.js"
+  },
+  {
+    "revision": "a93d8e1a89f9100d7e04c98ebcbc0933",
+    "url": "/static/media/bg.a93d8e1a.mp3"
+  },
+  {
+    "revision": "8d516cce9b80ee257214da40ace5c474",
+    "url": "/static/media/click.8d516cce.mp3"
+  },
+  {
+    "revision": "12d1603454265a90269ca13e0d6c04b2",
+    "url": "/static/media/earth.12d16034.jpg"
+  },
+  {
+    "revision": "e896d081c42d144db443a0faeea36cf8",
+    "url": "/static/media/engine.e896d081.mp3"
+  },
+  {
+    "revision": "bdb080913490e33da93bb6510d530d4d",
+    "url": "/static/media/engine2.bdb08091.mp3"
+  },
+  {
+    "revision": "5c5ce6974ee4192d9ecbc74242daa4ce",
+    "url": "/static/media/explosion.5c5ce697.mp3"
+  },
+  {
+    "revision": "a97c8e670e85efbd49691d7e343da830",
+    "url": "/static/media/laser.a97c8e67.mp3"
+  },
+  {
+    "revision": "8e294414ed8fefd6cc5fe6480adeaa7b",
+    "url": "/static/media/moon.8e294414.png"
+  },
+  {
+    "revision": "5699f1c4aff5845551ef0ba1a7ad31f8",
+    "url": "/static/media/warp.5699f1c4.mp3"
+  }
+]);
